b31ddf64f3e354aabe7c27d00150c69d48d9783a
nataliavera6 <natalia.vera6@upr.edu>
2024-04-12 15:38:40 -0400

    station class

--- src/main/Station.java
+++ src/main/Station.java
@@ -2,21 +2,27 @@ package main;
 
 public class Station {
 	
-	
+	private String name;
+	private int dist;
 	public Station(String name, int dist) {
+		this.name=name;
+		this.dist=dist;
 	}
 	
 	public String getCityName() {
+		return this.name;
 		
 	}
 	public void setCityName(String cityName) {
+		this.name=cityName;
 		
 	}
 	public int getDistance() {
+		return this.dist;
 		
 	}
 	public void setDistance(int distance) {
-		
+		this.dist=distance;
 	}
 
 	@Override
